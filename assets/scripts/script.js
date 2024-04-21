49b70581ec2f532ae1f47663f2bef1dabf0be0ea
Chael <devin.chael@gmail.com>
2024-04-22 03:55:28 +0800

    Added Comments on script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -71,25 +71,35 @@ $(document).ready(function(){
 
     $('.js-add-btn').on('click', function(e1){
         e1.preventDefault();
+
+        //Set Item Data
         let dataId    = $(this).attr('data-id');
         let itemTitle = $(this).attr('data-title');
         let itemPrice = $(this).attr('data-price');
         let itemImage = $(this).attr('data-image');
 
+        //Convert Data to Array
         let productDetails = {"title": itemTitle, "price": itemPrice, "image": itemImage, "id":dataId};
 
+        //Convert array to string
         let stringProductDetails = JSON.stringify(productDetails)
 
+        //Save data to localstorage
         localStorage.setItem(`id-${dataId}`, stringProductDetails);
 
+        //Get ID
         let getTitle = localStorage.getItem(`id-${dataId}`);
 
+        //Convert string to Array
         let parseProduct = JSON.parse(getTitle);
 
+        //Check if working
         console.log(parseProduct['title']);
 
+        //Update Cart Number
         $('.cart-number').html(localStorage.length);
 
+        //Append Item to Cart Modal
         $('.cart-modal ul').append(
             `
             <li class="cart-list-${parseProduct['id']}">
@@ -102,6 +112,7 @@ $(document).ready(function(){
             `
         );
 
+        //Check if Item is already added then change button from buy item to remove item button
         if (localStorage.getItem(`id-${dataId}`) === null){
             $(`.js-add-btn-${dataId}`).fadeIn();
             $(`.js-remove-btn-${dataId}`).fadeOut();
@@ -114,17 +125,22 @@ $(document).ready(function(){
     $('.js-remove-btn').on('click', function(e2){
         e2.preventDefault();
 
+        //Set Item Data
         let dataId    = $(this).attr('data-id');
         let itemTitle = $(this).attr('data-title');
         let itemPrice = $(this).attr('data-price');
         let itemImage = $(this).attr('data-image');
 
+        //Remove HTML of item
         $(`.cart-list-${dataId}`).remove();
 
+        //Remove from localstorage
         localStorage.removeItem(`id-${dataId}`);
 
+        //Update Cart Number
         $('.cart-number').html(localStorage.length);
 
+        //Check if Item is already added then change button from buy item to remove item button
         if (localStorage.getItem(`id-${dataId}`) === null){
             $(`.js-add-btn-${dataId}`).fadeIn();
             $(`.js-remove-btn-${dataId}`).fadeOut();
@@ -133,26 +149,26 @@ $(document).ready(function(){
             $(`.js-remove-btn-${dataId}`).fadeIn();
         }
 
-        
-
-        console.log(dataId);
-
     });
 
-
+    //Run Functions
     cartItems();
     cartPage();
 
+    //Remove Cart Items on Cart Page
     $('.remove-btn-cart').on('click', function(remove){
         remove.preventDefault();
 
+        //Get Data
         let dataId    = $(this).attr('data-id');
         let itemTitle = $(this).attr('data-title');
         let itemPrice = $(this).attr('data-price');
         let itemImage = $(this).attr('data-image');
 
+        //Remove HTML of item 
         $(`.cart-list-${dataId}`).remove();
 
+        //Remove from Local Storage
         localStorage.removeItem(`id-${dataId}`);
 
         $('.cart-number').html(localStorage.length);
@@ -160,16 +176,17 @@ $(document).ready(function(){
 
         console.log(dataId);
 
-
+        //Set total Price
         totalPrice = 0;
         
+        //Loop through and Add the integers
         for (i=0; i<localStorage.length; i++){
             let key = localStorage.key(i);  
             let parseProduct = JSON.parse(localStorage.getItem(key));
             totalPrice += parseFloat(parseProduct['price']);
             
         }
-
+        //Final Price
         $('.sum').html(`$${totalPrice.toFixed(2)}`);
 
     });
